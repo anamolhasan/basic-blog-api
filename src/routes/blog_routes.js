167ccb2d5c91b8c2ee93b2413174d2a875f0a88b
anamolhasan <mdanamolhasan1998@gmail.com>
2025-07-24 22:24:26 +0600

    blog update route added

--- src/routes/blog_routes.js
+++ src/routes/blog_routes.js
@@ -46,4 +46,33 @@ router.post('/add-post', (req, res) => {
   })
 })
 
+// update blog
+router.put('/update-post/:id', (req, res) => {
+    const {id} = req.params
+    const {title, description, author} = req.body
+    
+    const blog = blogs.find((blog) => blog.id === parseInt(id))
+       if(!blog) return res.status(404).json({message: 'blog not found'})
+       
+        blog.title = title || blog.title
+        blog.description = description || blog.description
+        // blog.author = author || blog.author
+
+        res.status(200).json({
+          message: 'Blog updated successfully',
+          blog
+        })
+})
+
+// delete blog
+router.delete('/delete-post/:id', (req, res) => {
+  const {id} = req.params
+
+  const blogIndex = blogs.findIndex((blog) => blog.id === parseInt(id))
+  if(!blogIndex === -1) return res.send(400).json({message: 'Blog not found!'})
+
+    blogs.splice(blogIndex, 1)
+    res.status(200).json({message: 'Blog delete successfully!'})
+})
+
 module.exports = router;

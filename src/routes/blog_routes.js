dc445043e0723a0346a393ccc784e461be062285
anamolhasan <mdanamolhasan1998@gmail.com>
2025-07-24 15:30:37 +0600

    blog route post route added

--- src/routes/blog_routes.js
+++ src/routes/blog_routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const blog = [
+const blogs = [
   {
     id: 1,
     title: "this first is title",
@@ -19,8 +19,31 @@ const blog = [
   },
 ];
 
-// router.get('/', (req, res) => {
-//     res.send('this is form blog route')
-// })
+router.get('/', (req, res) => {
+    res.send(blogs)
+})
+
+router.get('/:id', (req, res) => {
+  const {id} = req.params
+  
+  const existingBlog = blogs.find((blog) => blog.id === parseInt(id))
+
+  if(!existingBlog) {
+    return res.status(404).send({message:'No blog found'})
+  }
+
+  res.status(200).send(existingBlog)
+})
+
+// add a new blog
+router.post('/add-post', (req, res) => {
+  const {title, description} = req.body
+  const newBlog = {id: blogs.length + 1, title, description }
+  blogs.push(newBlog)
+  res.status(200).json({
+    message: 'new blog successfully',
+    blog: newBlog
+  })
+})
 
 module.exports = router;
